e44e154ac23a32992012afe9f5fc57af3e328927
BarretteBanner <barrettebanner@gmail.com>
2019-11-20 07:55:19 -0600

    done

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import {View, Text, TextInput, StyleSheet, Button} from 'react-native'
+
+const BlogPostForm = ({onSubmit, initialValues}) => {
+    const[title,setTitle] = useState(initialValues.title)
+    const [content, setContent] = useState(initialValues.content)
+    return (
+        <View>
+            <Text style={styles.label}>Enter title:</Text>
+            <TextInput value={title} onChangeText={(text) => setTitle(text)} style={styles.input}/>
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput value={content} onChangeText={(text) => setContent(text)} style={styles.input}/>
+            <Button title='Save Blog Post' onPress={() => onSubmit(title, content)} />
+        </View>
+    )
+}
+
+BlogPostForm.defaultProps = {
+    initialValues: {
+        title: '',
+        content: ''
+    }
+}
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 10,
+        marginLeft: 5
+    }
+})
+
+export default BlogPostForm
